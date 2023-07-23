e2b3785492427ca65123f464f227156400ed8697
MatthewCharlton <33848730+MatthewCharlton@users.noreply.github.com>
2023-07-23 12:35:34 +1000

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,28 +1,30 @@
-export { }
+export {};
 
 type Result<T> = {
-    error: unknown;
-    value: T
-}
+    error: unknown | undefined;
+    value: T | undefined;
+};
 
 declare global {
     interface Promise<T> {
-        unwrap(...args: any): Promise<Result<T>>
+        unwrap(...args: any): Promise<Result<T>>;
     }
 }
 
-Promise.prototype.unwrap = async function <T>(...args: any) {
+Promise.prototype.unwrap = async function <T>(
+    ...args: any
+) {
     let res = undefined;
     let err = undefined;
 
     try {
-        res = await this.then<T>(...args)
+        res = await this.then<T>(...args);
     } catch (e) {
-        err = e
+        err = e;
     }
 
     return {
         value: res,
-        error: err
-    }
-}
+        error: err,
+    };
+};
